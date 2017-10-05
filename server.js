0afc9a5138b64466d125e48393ee2fe693455aca
ismaila sylla <syldox@gmail.com>
2017-10-05 14:08:38 +0530

    nodejs-first commit

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const hbs = require('hbs');
+
+
+var app = express();
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+
+// middleware
+app.use((req, res, next) =>{
+    var now = new Date().toString();
+
+    console.log(`${now}:`);
+    next();
+
+});
+
+// maintenancemode
+
+app.use((req,res, next)=>{
+    res.render('maintenance.hbs');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+
+// home page
+app.get('/', (req, res)=>{
+    res.render('home.hbs', {
+        pageTitle:'Home',
+        welcomeMsg:'Welcome to the Home Page who ever you are !',
+        currentYear: new Date().getFullYear()
+    });
+});
+
+
+// about page
+app.get('/about', (req, res) =>{
+    res.render('about.hbs',{
+        pageTitle:'About',
+        currentYear: new Date().getFullYear()
+    });
+});
+
+
+
+
+// server port
+app.listen(3000, ()=>{
+    console.log('server is up on port 3000!');
+
+});
